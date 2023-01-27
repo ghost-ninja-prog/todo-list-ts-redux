5037894e81aa644cef912f75ad266ca8fdbc9309
ghost-ninja-prog <alex19892140@inbox.ru>
2023-01-28 02:01:23 +0700

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './styles/style.css';
 
 function App() {
   return (
-    <div >
-      <h1>Hello TypeScript</h1>
+    <div className='center'>
+      <h1>Hello TypeScript!!</h1>
     </div>
   );
 }
